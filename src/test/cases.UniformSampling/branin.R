a0d7749764d51b69c11a0df898612c2aa8208174
Yann Richet <richetyann@gmail.com>
2022-11-25 22:08:18 +0100

    Update branin.R

--- src/test/cases.UniformSampling/branin.R
+++ src/test/cases.UniformSampling/branin.R
@@ -6,9 +6,9 @@
 # [print.f] : method to print/plot the function for information
 
 f <- function(x) {
-	x1 <- x[,1]*15-5   
-	x2 <- x[,2]*15     
-	matrix((x2 - 5/(4*pi^2)*(x1^2) + 5/pi*x1 - 6)^2 + 10*(1 - 1/(8*pi))*cos(x1) + 10,ncol=1)
+	x1 <- x[1]*15-5   
+	x2 <- x[2]*15     
+	(x2 - 5/(4*pi^2)*(x1^2) + 5/pi*x1 - 6)^2 + 10*(1 - 1/(8*pi))*cos(x1) + 10
 }
 input.f = list(
     x1=list(min=0,max=1),
